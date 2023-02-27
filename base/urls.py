dc46869b46a68777d3e96d370b64d898c23de04c
DWK <you@example.com>
2023-02-27 11:20:20 +0900

    20230227

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
