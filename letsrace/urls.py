b63e3ccda9cc0418865441128fdbf8db967350be
DWK <you@example.com>
2023-04-12 10:31:51 +0900

    20230412 mobile - 2

--- letsrace/urls.py
+++ letsrace/urls.py
@@ -31,9 +31,11 @@ urlpatterns = [
 
     path('', include('base.urls')),
 
+    # url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
 
 
 ]
 
-# //urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
