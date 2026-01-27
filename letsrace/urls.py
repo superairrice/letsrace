e2a4c4748fce88f81c6fb8ec2ecd7d2e806f6f04
DWK <you@example.com>
2026-01-27 17:35:07 +0900

    20260127 mobile - 7

--- letsrace/urls.py
+++ letsrace/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
+
+def healthz(request):
+    return HttpResponse("ok")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
